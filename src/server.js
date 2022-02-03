4a811e1bac3ef405955adfe8905641493de2045e
Kyu-d <ldk0421@naver.com>
2022-02-04 00:19:11 +0900

    Create Server & Set PORT

--- src/server.js
+++ src/server.js
@@ -1,3 +1,11 @@
 import express from "express";
 
+const PORT = 4000;
+
 const app = express(); // Create Express Application
+
+const handleListening = () => console.log(`Server listening on port http://localhose:${PORT}`);
+
+app.listen(PORT, handleListening);
+
+// request & response
